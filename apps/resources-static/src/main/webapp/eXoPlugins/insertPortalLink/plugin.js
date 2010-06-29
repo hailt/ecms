1f1578f13256c6039e4f449944c67d4c2d9c1924
Lam Tuan Nguyen <lam.nguyen@exoplatform.com>
2010-06-29 09:59:03 +0000

    ECMS-497,Update old WCM's plugins to make them work with CKEditor

---
+++ apps/resources-static/src/main/webapp/eXoPlugins/insertPortalLink/plugin.js
@@ -0,0 +1,24 @@
+CKEDITOR.plugins.add('insertPortalLink',
+	{
+		init : function(editor) {
+			var pluginName = 'InsertPortalLink';
+			var mypath = this.path;	
+			editor.ui.addButton(
+				'insertPortalLink.btn',
+				{
+					label : "WCM Insert Portal Link",
+					command : 'insertPortalLink.cmd',
+					icon : mypath + '/images/insertPortalLink.gif'
+				}
+			);
+			var cmd = editor.addCommand('insertPortalLink.cmd', {exec:showInsertPortalLink});
+			cmd.modes = {wysiwyg: 1, source: 1};
+			cmd.canUndo = false;	
+			CKEDITOR.insertPortalLink = editor;
+		}
+	}
+);
+
+function showInsertPortalLink(e){
+	window.open(CKEDITOR.eXoPath+'eXoPlugins/insertPortalLink/insertPortalLink.html', 'WCM INSERT PORTAL LINK', 'width=600, height=400, scroll=no, resizable=yes');
+}
